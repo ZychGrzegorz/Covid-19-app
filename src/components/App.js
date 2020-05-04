b0b2ecdc747513f04ab60545dfe6a66d1ae29684
Grzegorz Zych <grzegorzzych95@gmail.com>
2020-05-04 11:55:43 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import logo from "./logo.svg";
+import "../scss/App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>U mnie dziala</h1>
+    </div>
+  );
+}
+
+export default App;
