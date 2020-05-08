8978dace7144616cc6fdcd8f1c8e9b9f6c3588b3
Grzegorz Zych <grzegorzzych95@gmail.com>
2020-05-08 12:43:47 +0200

    Updates

---
+++ precache-manifest.27eeaa275671c3229515468f3f5a22b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b3c20d4b8a30ae1ca44330bb985873c",
+    "url": "/Covid-19-app/index.html"
+  },
+  {
+    "revision": "7926e55be7cec4ceee6e",
+    "url": "/Covid-19-app/static/css/2.e47f0c24.chunk.css"
+  },
+  {
+    "revision": "f835c01796b7d7235b9b",
+    "url": "/Covid-19-app/static/css/main.7603bb45.chunk.css"
+  },
+  {
+    "revision": "7926e55be7cec4ceee6e",
+    "url": "/Covid-19-app/static/js/2.3d673495.chunk.js"
+  },
+  {
+    "revision": "0a572ad323474bafc6a871b6336141bd",
+    "url": "/Covid-19-app/static/js/2.3d673495.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f835c01796b7d7235b9b",
+    "url": "/Covid-19-app/static/js/main.f4aa5c1e.chunk.js"
+  },
+  {
+    "revision": "6d7e4ed477471214d9ac",
+    "url": "/Covid-19-app/static/js/runtime-main.a0d036d0.js"
+  }
+]);
